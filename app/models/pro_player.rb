73adae2245c2a73c3a040264c145bfb2fca03565
him666 <all.existance@gmail.com>
2017-01-03 19:42:12 -0600

    changed self methods in proplayer

--- app/models/pro_player.rb
+++ app/models/pro_player.rb
@@ -3,49 +3,7 @@ class ProPlayer < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  def self.cs_prom(id)
-    pro = ProPlayer.find(id)
-    cs_num = 0
-    pro.game_stats.each do |game|
-      cs_num += game.minions
-    end
-    cs_num/20
+  def self.average(id,column)
+    ProPlayer.find(id).game_stats.average(column).to_i
   end
-
-  def self.k_prom(id)
-    pro = ProPlayer.find(id)
-    k_prom = 0
-    pro.game_stats.each do |game|
-      k_prom += game.kills
-    end
-    k_prom/20
-  end
-
-  def self.d_prom(id)
-    pro = ProPlayer.find(id)
-    d_prom = 0
-    pro.game_stats.each do |game|
-      d_prom += game.deaths
-    end
-    d_prom/20
-  end
-
-  def self.a_prom(id)
-    pro = ProPlayer.find(id)
-    a_prom = 0
-    pro.game_stats.each do |game|
-      a_prom += game.assists
-    end
-    a_prom/20
-  end
-
-  def self.w_prom(id)
-    pro = ProPlayer.find(id)
-    w_prom = 0
-    pro.game_stats.each do |game|
-      w_prom += game.wards
-    end
-    w_prom/20
-  end
-
 end
---
+++ app/workers/sum_spell.rb
@@ -0,0 +1,3 @@
+class SumSpell
+  # populate Summoner's Spells db 
+end
